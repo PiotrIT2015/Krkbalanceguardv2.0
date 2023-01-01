49cded8b03c9b45f782f1e1c08d890bef1254446
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-01-01 19:21:20 +0100

    Feature-fix/add daolayer to reminder services

---
+++ io.krkbalanceguard.services/reminder/src/main/java/engine/pp/model/Person.java
@@ -0,0 +1,25 @@
+package engine.pp.model;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="dd/MM/yyyyy")
+    private Date dob;
+
+}

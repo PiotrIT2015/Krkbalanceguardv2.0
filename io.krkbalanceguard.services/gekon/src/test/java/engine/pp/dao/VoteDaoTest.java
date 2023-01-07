7f0941c32fea774efc52b2a5eaa99ff30789c1ac
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-01-07 22:38:54 +0100

    Feature-fix/rename file

---
+++ io.krkbalanceguard.services/gekon/src/test/java/engine/pp/dao/VoteDaoTest.java
@@ -0,0 +1,30 @@
+package engine.pp.dao;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VoteDaoTest {
+
+    private VoteDao votedao;
+
+    @BeforeEach
+    void setUp() {
+        votedao=new VoteDao();
+    }
+
+    @Test
+    void getPath() {
+        votedao.setPath("./data/resolution-list.csv");
+        assertEquals("./data/resolution-list.csv",votedao.getPath());
+    }
+
+    /*
+    @Test
+    void getItem() {
+        votedao.setItem("resolution1");
+        assertEquals("resolution1",votedao.getItem());
+    }
+     */
+}

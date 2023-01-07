d1858f19d73f95459b625ac12c29582e8b662fb0
Piotr Próchnicki <piotr.prochnicki@gmail.com>
2023-01-07 15:35:59 +0100

    Feature-fix/add a service and a controller

---
+++ io.krkbalanceguard.services/reminder/src/main/java/engine/pp/controller/personController.java
@@ -0,0 +1,28 @@
+package engine.pp.controller;
+
+import engine.pp.dao.PersonDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/spring-boot-orm")
+public class personController {
+
+    @Autowired
+    private PersonDao dao;
+
+    @RequestMapping("/saveVote")
+    public String save(@RequestBody String vote){
+        dao.saveShopping(vote);
+        dao.getItem();
+        return "success";
+    }
+/*
+    @GetMapping("/getAll")
+    public List<Vote> getAllVote(){
+        return dao.getVote();
+    }
+*/
+}
--- io.krkbalanceguard.services/reminder/src/main/java/engine/pp/dao/PersonDao.java
+++ io.krkbalanceguard.services/reminder/src/main/java/engine/pp/dao/PersonDao.java
@@ -1,11 +1,25 @@
 package engine.pp.dao;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
 public class PersonDao {
 
+    String items;
     @Autowired
     private SessionFactory factory;
 
@@ -24,6 +38,50 @@ public class PersonDao {
             session=factory.openSession();
         }
         return session;
+    }
+
+    @Bean
+    public void getItem(){
+        String csvFile = "./data/shopping-list.csv";
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ",";
+        ArrayList<String> shoppings = new ArrayList<String>();
+        try {
+            br = new BufferedReader(new FileReader(csvFile));
+            while ((line = br.readLine()) != null) {
+                // use comma as separator
+                String[] shopping = line.split(cvsSplitBy);
+                System.out.println(" [law= " + shopping[0] + "]");
+                for(int i=0;i<=shopping.length-1;i++)
+                {
+                    String resolution[] = shopping[2].split(cvsSplitBy);
+                    String resolutionslist = resolution[i] + resolution[i+1];
+                    shoppings.add(resolutionslist);
+                }
+                Iterator itr1 = shoppings.iterator();
+                while(itr1.hasNext()) {
+                    Object resolutionslist = itr1.next();
+                    int resolutionslists = (int)resolutionslist;
+                    items=resolutionslist.toString();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        saveShopping(items);
+
     }
 
 }
